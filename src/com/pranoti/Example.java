450a575821138fe18363a676bf18d205373fb26c
Pranoti Naiknaware <pranotinaiknaware@gmail.com>
2024-05-27 14:50:35 +0530

    My second project

---
+++ src/com/pranoti/Example.java
@@ -0,0 +1,21 @@
+package com.pranoti;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Example em=new Example();
+em.m1();
+	}
+	public void m1() {
+		List<String>list=new ArrayList<String>();
+		list.add("Core java");
+		list.add("collection framework");
+		list.add("MySql");
+		list.add("Git");
+		System.out.println(list);
+	}}
+
