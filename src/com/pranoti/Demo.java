de6d16e8d7abb35499e63219d54741d01037581d
Pranoti Naiknaware <pranotinaiknaware@gmail.com>
2024-05-27 15:35:44 +0530

    Second commit

--- src/com/pranoti/Demo.java
+++ src/com/pranoti/Demo.java
@@ -3,12 +3,11 @@ package com.pranoti;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
+		
 System.out.println("Hello pranoti");
-=======
+
 System.out.println("Hello to");
->>>>>>> 33b359b4b7eeeb3043e93256d74abb0174532874
+
 	}
 
 }
